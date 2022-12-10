b0faa93c7651c0129645d5c33403b346c2f1d065
Gennadiy <glisovuy@ukr.net>
2022-12-10 11:15:42 +0200

    add React Fragment

--- src/index.tsx
+++ src/index.tsx
@@ -10,21 +10,46 @@ import ReactDOM from 'react-dom/client'
 //without JSX
 // const h1 = React.createElement("h1", {id: 'title', className:'test'}, "Hello React")
 
-//Component повертають зовнішній вигляд. назва з великої! 
+//Component повертають зовнішній вигляд. назва з великої!
 
-function App() {
-return (
-  <div>
-    <h1>
-      Hello React.js
-    </h1>
-    <p>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam doloribus saepe magnam, non quas quod placeat tenetur velit, ab vero laboriosam voluptate consequuntur laborum quasi perferendis soluta provident fuga eaque.
-    </p>
-  </div>
-)
+const Title = () => {
+    return <h1>Hello React.js</h1>
+}
+const Content = () => {
+    return (
+        <React.Fragment>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam
+                doloribus saepe magnam, non quas quod placeat tenetur velit, ab
+                vero laboriosam voluptate consequuntur laborum quasi perferendis
+                soluta provident fuga eaque. Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Odio ratione nesciunt molestiae
+                vitae laborum ea dolores fugiat nihil? In eveniet recusandae
+                adipisci asperiores quis natus debitis voluptate aperiam quae
+                quibusdam?
+            </p>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam
+                doloribus saepe magnam, non quas quod placeat tenetur velit, ab
+                vero laboriosam voluptate consequuntur laborum quasi perferendis
+                soluta provident fuga eaque.
+            </p>
+        </React.Fragment>
+    )
+}
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <Title />
+            <Content />
+        </React.Fragment>
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode><App></App></React.StrictMode>)
-
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
