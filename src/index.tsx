3931755a3e9c3e240156092bbb237e30b1b98abc
Gennadiy <glisovuy@ukr.net>
2022-12-10 10:46:42 +0200

    add element

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
+import { title } from 'process'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// JSX
+// const h1 = <h1 id="title" className="test">Hello world</h1>
+
+//without JSX
+const h1 = React.createElement("h1", [id: 'title', className:'test'], "Hello React")
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(<React.StrictMode>{h1}</React.StrictMode>)
 
