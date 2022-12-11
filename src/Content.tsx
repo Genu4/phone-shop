570d870f23bfda1a5bbda27633931cf56cdc0f4f
Gennadiy <glisovuy@ukr.net>
2022-12-11 10:22:30 +0200

    іменований та дефолтний імпорт

---
+++ src/Content.tsx
@@ -0,0 +1,24 @@
+export const Content = () => {
+    return (
+        <>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam
+                doloribus saepe magnam, non quas quod placeat tenetur velit, ab
+                vero laboriosam voluptate consequuntur laborum quasi perferendis
+                soluta provident fuga eaque. Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Odio ratione nesciunt molestiae
+                vitae laborum ea dolores fugiat nihil? In eveniet recusandae
+                adipisci asperiores quis natus debitis voluptate aperiam quae
+                quibusdam?
+            </p>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam
+                doloribus saepe magnam, non quas quod placeat tenetur velit, ab
+                vero laboriosam voluptate consequuntur laborum quasi perferendis
+                soluta provident fuga eaque.
+            </p>
+        </>
+    )
+}
+
+// export default Content
--- src/index.tsx
+++ src/index.tsx
@@ -1,56 +1,27 @@
 import { title } from 'process'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {Content} from './Content'
 
-//Element
-
-// JSX
-// const h1 = <h1 id="title" className="test">Hello world</h1>
-
-//without JSX
-// const h1 = React.createElement("h1", {id: 'title', className:'test'}, "Hello React")
-
-//Component повертають зовнішній вигляд. назва з великої!
 type TitleProps = {
-    
     title: string
     text?: string
     num?: number
 }
 
-
-const Title = ({title, text='Test', num}: TitleProps) => {
-    return <h1>{text} {title} {num}</h1>
-}
-const Content = () => {
+const Title = ({ title, text = 'Test', num }: TitleProps) => {
     return (
-        <>
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam
-                doloribus saepe magnam, non quas quod placeat tenetur velit, ab
-                vero laboriosam voluptate consequuntur laborum quasi perferendis
-                soluta provident fuga eaque. Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Odio ratione nesciunt molestiae
-                vitae laborum ea dolores fugiat nihil? In eveniet recusandae
-                adipisci asperiores quis natus debitis voluptate aperiam quae
-                quibusdam?
-            </p>
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam
-                doloribus saepe magnam, non quas quod placeat tenetur velit, ab
-                vero laboriosam voluptate consequuntur laborum quasi perferendis
-                soluta provident fuga eaque.
-            </p>
-        </>
+        <h1>
+            {text} {title} {num}
+        </h1>
     )
 }
+
 
 const App = () => {
     return (
         <React.Fragment>
-            <Title text="Hello" title="React" num = {10}/>
-            <Title text="Hi" title="React.js" />
-            <Title title="Bla-bla" />
+            <Title text="Hello" title="React" />
             <Content />
         </React.Fragment>
     )
