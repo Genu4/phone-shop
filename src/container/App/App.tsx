a118a76f1e89e6601d3d51f06514c11904ef1763
Gennadiy <glisovuy@ukr.net>
2022-12-17 10:53:08 +0200

    change style with StyleEngineProvider

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,14 +1,17 @@
-import Header from "container/Header/Header"
-import CssBaseline from '@mui/material/CssBaseline';
+import Header from 'container/Header/Header'
+import CssBaseline from '@mui/material/CssBaseline'
+import { StyledEngineProvider } from '@mui/material/styles'
 
 type Props = {}
 
 const App = (props: Props) => {
-  return (
-    <div>
-        <CssBaseline />
-        <Header />
-    </div>
-  )
+    return (
+        <div>
+            <StyledEngineProvider injectFirst>
+                <CssBaseline />
+                <Header />
+            </StyledEngineProvider>
+        </div>
+    )
 }
 export default App
