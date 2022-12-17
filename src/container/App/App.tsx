fd81a22e26f303417fc31fb998efc00de55697f5
Gennadiy <glisovuy@ukr.net>
2022-12-17 10:20:03 +0200

    instal MUI

---
+++ src/container/App/App.tsx
@@ -0,0 +1,14 @@
+import Header from "container/Header/Header"
+import CssBaseline from '@mui/material/CssBaseline';
+
+type Props = {}
+
+const App = (props: Props) => {
+  return (
+    <div>
+        <CssBaseline />
+        <Header />
+    </div>
+  )
+}
+export default App
