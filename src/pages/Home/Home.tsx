406e9cdfb1038161e965cb9ba30416ec623f2a6c
Gennadiy <glisovuy@ukr.net>
2022-12-18 10:20:03 +0200

    add products

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import { Typography } from '@mui/material'
+import ProductsList from 'components/Products/ProductsList'
 
 type Props = {}
 
@@ -6,10 +7,9 @@ const Home = (props: Props) => {
     return (
         <>
             <Typography variant="h4" align="center">
-                {''}
                 List of Products
-                {''}
             </Typography>
+            <ProductsList />
         </>
     )
 }
